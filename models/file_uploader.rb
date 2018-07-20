c7c8e24e9b0789e82fc7fd812a8ef1eff491b270
Joao Cunha <j.a.cunha@gmail.com>
2018-07-20 23:50:30 +0200

    adds method to generate s3 file path

--- models/file_uploader.rb
+++ models/file_uploader.rb
@@ -4,4 +4,8 @@ class FileUploader < Shrine
   plugin :upload_options, store: (lambda { |_io, _context|
     { acl: "public-read" }
   })
+
+  def self.s3_path(file_id)
+    "https://s3.#{ENV['S3_REGION']}.amazonaws.com/#{ENV['S3_BUCKET']}/#{file_id}"
+  end
 end
