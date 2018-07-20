5055beb8dc9fa74b8eb8341a9989339a981a0810
Joao Cunha <j.a.cunha@gmail.com>
2018-07-20 18:08:53 +0200

    create account model

---
+++ spec/models/account_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+RSpec.describe Account do
+  xit "is not valid without an email" do
+
+  end
+
+  xit "is not valid without a name" do
+
+  end
+
+  xit "is not valid without a password" do
+
+  end
+end
